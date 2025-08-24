0c4c071d07b6bd01700eabaef92cb39cebc4d16c
NeilR2s <neil.c.artus@gmail.com>
2025-08-24 21:55:44 +0800

    css and js boilerplate

---
+++ static/script.js
@@ -0,0 +1,16 @@
+// document.addEventListener("DOMContentLoaded", () => {
+
+//     const toggleButton = document.getElementById("toggle-button");
+//     const appContainer = document.querySelector(".app-container");
+//     const collapseIcon = document.querySelector(".icon-collapse");
+//     const exapandIcon = document.querySelector(".icon-expand");
+
+//     const toggleSidebar = () => {
+//         collapseIcon.style.display = "None";
+//     };
+
+//     toggleButton.addEventListener("click", toggleSidebar);
+
+
+// });
+
