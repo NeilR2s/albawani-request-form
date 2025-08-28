273b870097a2d074e3d9392a77086e31f92bab25
NeilR2s <neil.c.artus@gmail.com>
2025-08-28 13:31:17 +0800

    add client side validation

--- static/script.js
+++ static/script.js
@@ -1,38 +1,53 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const toggleBtn = document.getElementById('toggle-button'); 
-    const appContainer = document.querySelector('.app-container');
-    const collapseIcon = document.querySelector('.icon-collapse');
-    const expandIcon = document.querySelector('.icon-expand');
+const requestAttendanceBtn = document.getElementById("request-attendance-btn");
+const startDateForm = document.getElementById("start-date");
+const endDateForm = document.getElementById("end-date");
+const notifMessagePopup = document.getElementById("notification-message-popup");
+const notifMessage = document.getElementById("notification-message");
 
-    // must hide the expand icon if the sidebar is open
-    if (!appContainer.classList.contains('sidebar-collapsed')) {
-        expandIcon.style.display = 'none';
+
+const notificationDuration = 3000;
+
+function getDateRange(){
+
+    let startDate = startDateForm.value
+    let endDate = endDateForm.value
+    let dateSuccessMessage = `Successfully sent attendance request`;
+    let dateWarningMessage = `Warning: your given end date ${endDate} is the same as your start date ${startDate}`;
+    let dateErrorMessage = `Error: your given end date ${endDate} must be greater than your start date ${startDate}`;
+    console.log(`START: ${startDate} || END: ${endDate}`)
+
+    notifMessagePopup.classList.remove('success', 'warning', 'error');
+
+    if (endDate == startDate){
+        notifMessagePopup.classList.add("warning")
+        notifMessagePopup.classList.add("visible")
+        notifMessage.innerHTML = dateWarningMessage
+        // console.warn(`your given end date ${endDate} is the same as your start date ${startDate}`);
     }
 
-    // switch css selector to show only a bit
-    const toggleSidebar = () => {
-        appContainer.classList.toggle('sidebar-collapsed');
-        
-        // must change to icons when collapsed
-        if (appContainer.classList.contains('sidebar-collapsed')) {
-            localStorage.setItem('sidebarState', 'collapsed');
-            collapseIcon.style.display = 'none';
-            expandIcon.style.display = 'block';
-        } else {
-            localStorage.setItem('sidebarState', 'open');
-            collapseIcon.style.display = 'block';
-            expandIcon.style.display = 'none';
-        }
-    };
-    
-    const savedState = localStorage.getItem('sidebarState');
-    if (savedState === 'collapsed') {
-        appContainer.classList.add('sidebar-collapsed');
-        collapseIcon.style.display = 'none';
-        expandIcon.style.display = 'block';
+    else if (endDate < startDate){
+        notifMessagePopup.classList.add("error")
+        notifMessagePopup.classList.add("visible")
+        notifMessage.innerHTML = dateErrorMessage
+        // console.error(`your given end date ${endDate} must be greater than your start date ${startDate}`)
     }
+    else {
+        notifMessagePopup.classList.add("success")
+        notifMessagePopup.classList.add("visible")
+        notifMessage.innerHTML = dateSuccessMessage
+        // console.error(`your given end date ${endDate} must be greater than your start date ${startDate}`)
+    }
+};
 
-    if (toggleBtn) { 
-        toggleBtn.addEventListener('click', toggleSidebar);
-    }
+function clearDateNotification(){
+    notifMessagePopup.classList,remove("visible")
+    notifMessage.innerHTML = ""
+}
+
+requestAttendanceBtn.addEventListener('click', function() {
+    getDateRange();
+    setTimeout(() => {
+        clearDateNotification()
+    }, notificationDuration);
 });
+
