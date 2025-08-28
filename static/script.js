6781c546450ef93df1edf9577e018bb7f07f35c6
NeilR2s <neil.c.artus@gmail.com>
2025-08-28 20:12:04 +0800

    added attendance api call

--- static/script.js
+++ static/script.js
@@ -1,3 +1,8 @@
+const DEV_MODE = true; 
+
+const BACKEND_URL = `http://127.0.0.1:5000`;
+const ATTENDANCE_API = BACKEND_URL + `/request-attendance`;
+
 const requestAttendanceBtn = document.getElementById("request-attendance-btn");
 const startDateForm = document.getElementById("start-date");
 const endDateForm = document.getElementById("end-date");
@@ -7,8 +12,31 @@ const notifMessage = document.getElementById("notification-message");
 
 const notificationDuration = 3000;
 
-function getDateRange(){
+async function sendAttendanceRequest(attendanceEndpoint, attendanceDateRange) {
+    try {
+        const response = await fetch (attendanceEndpoint, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(attendanceDateRange) 
+        });
+        
+        const responseData = response.json()
+        if (!responseData.ok){
+            throw new Error(`idk something something: ${responseData}`)
+        }
+        console.log(`/attendance response data: ${responseData}`)
+    }
 
+    catch (error){
+        console.error(error)
+    }
+};
+
+function getDateRange(){
+    const getcurrentDate = new Date()
+    console.log(`Today: ${getcurrentDate}`)
     let startDate = startDateForm.value
     let endDate = endDateForm.value
     let dateSuccessMessage = `Successfully sent attendance request`;
@@ -25,12 +53,15 @@ function getDateRange(){
         notifMessage.innerHTML = dateWarningMessage
         // console.warn(`your given end date ${endDate} is the same as your start date ${startDate}`);
     }
-
+    else if (endDate>getcurrentDate){
+        console.log(`Selected end date big`)
+    }
     else if (endDate == "" || startDate =="") {
         notifMessagePopup.classList.add("error")
         notifMessagePopup.classList.add("visible")
         notifMessage.innerHTML = dateEmptyMessage
     }
+
     else if (endDate < startDate){
         notifMessagePopup.classList.add("error")
         notifMessagePopup.classList.add("visible")
@@ -43,6 +74,12 @@ function getDateRange(){
         notifMessage.innerHTML = dateSuccessMessage
         // console.error(`your given end date ${endDate} must be greater than your start date ${startDate}`)
     }
+
+    return {
+        "startDate": startDate,
+        "endDate": endDate,
+        "message": "hello from Albawani Client"
+    }
 };
 
 function clearDateNotification(){
@@ -51,9 +88,11 @@ function clearDateNotification(){
 }
 
 requestAttendanceBtn.addEventListener('click', function() {
-    getDateRange();
+    const filteredDateRange = getDateRange();
+    const attendanceServerResponse = sendAttendanceRequest(ATTENDANCE_API, filteredDateRange)
     setTimeout(() => {
         clearDateNotification()
     }, notificationDuration);
+    console.log(attendanceServerResponse)
 });
 
